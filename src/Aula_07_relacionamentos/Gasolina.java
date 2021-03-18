ea9ebf17a8e7759ad9c77ad2a2c9ad0deec6314d
David Ferreira de Almeida <72570193+da-ferreira@users.noreply.github.com>
2021-03-18 11:08:06 -0300

    Add files via upload

---
+++ src/Aula_07_relacionamentos/Gasolina.java
@@ -0,0 +1,18 @@
+
+package Aula_07_relacionamentos;
+
+/**
+ * @author david-ferreira
+ */
+
+public class Gasolina {
+    private double litros; 
+    
+    public Gasolina(double litros) {
+        this.litros = litros;
+    }
+
+    public double getLitros() {
+        return litros;
+    }
+}
