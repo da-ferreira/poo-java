719569ba8c27283f4d5eb708d632e2aabb24fa5b
David Ferreira de Almeida <ferreiradavid361@gmail.com>
2021-04-29 11:40:41 -0300

    Add files via upload

---
+++ POO-CC3A/src/Aula_12_Heranca/BankAccount.java
@@ -0,0 +1,86 @@
+
+package Aula_12_Heranca;
+
+/**
+ * @author david-ferreira
+ */
+
+public class BankAccount {
+    protected String owner;       // Nome do proprietário da conta.
+    protected int accountNumber;  // Numero da conta.
+    protected double balance;     // Saldo da conta.
+    protected String password;    // Senha da conta.
+
+    public BankAccount(String owner, int accountNumber, double balance, String password) {
+        this.owner = owner;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.password = password;
+    }
+    
+    /**
+     * Deposita dinheiro nesta conta.
+     * (Pós-condição: getBalance() >= 0.0)
+     * @param value a quantidade de dinheiro  a depositar.
+     * (Pré-condição: value >= 0.0)
+     */
+    public void deposit(double value) {
+        this.balance += value;
+    }
+    
+    public void withDraw(double value) {
+        this.balance -= value;
+    }
+    
+    /* Métodos de acesso */
+    
+    public String getOwner() {
+        return owner;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    /* Métodos modificadores */
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    /** 
+     * Verifica se dois objetos da classe são iguais, comparando cada um dos atributos.
+     * @param bank Objeto a ser verificado.
+     * @return true se forem iguais; false, caso contrário.
+     */
+    public boolean equals(BankAccount bank) {
+        return owner.equals(bank.getOwner()) && accountNumber == bank.getAccountNumber() &&
+                balance == bank.getBalance() && password.equals(bank.getPassword());
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Owner: %s | Balance: %.4f | AccountNumber: %d | Senha: %s", this.owner, this.balance, this.accountNumber, this.password);
+    }     
+}
+  
---
+++ POO-CC3A/src/Aula_12_Heranca/SavingsAccount.java
@@ -0,0 +1,24 @@
+
+package Aula_12_Heranca;
+
+/**
+ * @author david-ferreira
+ */
+
+public class SavingsAccount extends BankAccount {
+    private double interestRate;  // taxa de juros
+
+    public SavingsAccount(double interestRate, String owner, int accountNumber, double balance, String password) {
+        super(owner, accountNumber, balance, password);  // chamando o construtor da classe BankAccount
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+    
+    @Override
+    public double getBalance() {
+        return super.getBalance() * (1 + interestRate);
+    }
+}
