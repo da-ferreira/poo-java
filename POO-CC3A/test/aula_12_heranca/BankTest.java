b91f38ac6727d4c688efdd8c2932f0812da0f74b
David Ferreira de Almeida <ferreiradavid361@gmail.com>
2021-05-05 13:15:49 -0300

    update tests aula 12

--- POO-CC3A/test/aula_12_heranca/BankTest.java
+++ POO-CC3A/test/aula_12_heranca/BankTest.java
@@ -3,8 +3,7 @@ package aula_12_heranca;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import Aula_09_Colecoes.Bank;
-import Aula_09_Colecoes.BankAccount;
+import Aula_12_Heranca.Bank;
 import Aula_12_Heranca.SavingsAccount;
 
 /**
@@ -21,23 +20,15 @@ public class BankTest {
     @Test
     public void testeAddSavingsAccount() {
         Bank banco = new Bank();
-        
         assertNotNull(banco);
         
         SavingsAccount conta = new SavingsAccount(0.05, "Joao Carneiro", 123, 500.00, "abc123");
         banco.addAccount(conta);
         
-        assertEquals(1, banco.getAccounts().size());
-        assertEquals(123, banco.getAccounts().get(0).getAccountNumber());
-        assertEquals("Joao Carneiro", banco.getAccounts().get(0).getOwner());
-        assertEquals(500.0, banco.getAccounts().get(0).getBalance(), 0.0001);
-        
-        banco.addAccount(new BankAccount("Carlos Freitas Filho", 66, 89.63, "carlos555"));
-        
-        assertEquals(2, banco.getAccounts().size());
-        assertEquals(66, banco.getAccounts().get(1).getAccountNumber());
-        assertEquals("Carlos Freitas Filho", banco.getAccounts().get(1).getOwner());
-        assertEquals(89.63, banco.getAccounts().get(1).getBalance(), 0.0001);
+        assertEquals(0.05, conta.getInterestRate(), 0.0001);
+        assertEquals(525.0, conta.getBalance(), 0.0001);
+        assertEquals("Joao Carneiro", conta.getOwner());
+        assertEquals(123, conta.getAccountNumber());
     }
 }
     
